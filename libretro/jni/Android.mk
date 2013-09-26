68a712edb43e6cfccac02eed08788af7513f9497
LLeny <xxx.leny@gmail.com>
2013-09-26 10:33:56 +0200

    Fix Android build

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -17,7 +17,7 @@ endif
 
 LOCAL_MODULE    := libretro
 LOCAL_LDLIBS    += -lz -llog
-LOCAL_SRC_FILES    =  ../../lynx/Cart.cpp ../../lynx/Memmap.cpp ../../lynx/Mikie.cpp ../../lynx/Ram.cpp ../../lynx/Rom.cpp ../../lynx/Susie.cpp ../../lynx/System.cpp ../../libretro/libretro.cpp
+LOCAL_SRC_FILES    =  ../../lynx/lynxdec.cpp ../../lynx/Cart.cpp ../../lynx/Memmap.cpp ../../lynx/Mikie.cpp ../../lynx/Ram.cpp ../../lynx/Rom.cpp ../../lynx/Susie.cpp ../../lynx/System.cpp ../../libretro/libretro.cpp
 LOCAL_CXXFLAGS = -DANDROID -DARM -D__LIBRETRO__ -DHAVE_STRINGS_H -DHAVE_STDINT_H -DRIGHTSHIFT_IS_SAR
 LOCAL_C_INCLUDES = ../../ ../../lynx/ ../../libretro/
 
