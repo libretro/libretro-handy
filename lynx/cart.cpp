445f6141419b3f688fa68d28bcb994e3c5776dfb
Björn Spruck <bjoern{}spruck.net>
2018-05-10 18:26:08 +0200

    removed broken code for Howie images, added working code

--- lynx/cart.cpp
+++ lynx/cart.cpp
@@ -262,12 +262,19 @@ CCart::CCart(UBYTE *gamedata,ULONG gamesize)
       gCPUBootAddress=0;
 
       //
-      // Check if this is a headerless cart
-      // Headerless used for Howie type... but this cannot work...
-      mHeaderLess=TRUE;
-      for(int loop=0;loop<32;loop++)
-      {
-         if(mCartBank0[loop&mMaskBank0]!=0x00) mHeaderLess=FALSE;
+      // Check if this is a headerless cart, either 410 (EPYX_HEADER_NEW) or 512 (EPYX_HEADER_OLD) zeros
+      //
+      mHeaderLess=EPYX_HEADER_OLD;// old EPYX Type
+      for(int loop=0; loop<EPYX_HEADER_OLD; loop++) {
+         if(mCartBank0[loop&mMaskBank0]!=0x00) {
+            if(loop<EPYX_HEADER_NEW) {
+               mHeaderLess=0;// less than 410 zeros -> invalid
+               break;
+            } else {
+               mHeaderLess=EPYX_HEADER_NEW;// at least 410 -> new EPYX type
+               break;
+            }
+         }
       }
       TRACE_CART1("CCart() - mHeaderLess=%d",mHeaderLess);
    }
