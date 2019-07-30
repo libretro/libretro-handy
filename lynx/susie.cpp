b2328b9ab444c0c9ed292cfafae20f4abb7ef708
snes2600 <52765182+snes2600@users.noreply.github.com>
2019-07-30 16:45:42 -0500

    fix: ms. pac-man speedup
    2 nybble pixels = 1/1 r/w pair; average 1 pixel penalty

--- lynx/susie.cpp
+++ lynx/susie.cpp
@@ -1257,7 +1257,7 @@ inline void CSusie::WritePixel(ULONG hoff,ULONG pixel)
    RAM_POKE(scr_addr,dest);
 
    // Increment cycle count for the read/modify/write
-   cycles_used+=2*SPR_RDWR_CYC;
+   //cycles_used+=2*SPR_RDWR_CYC;
 }
 
 inline ULONG CSusie::ReadPixel(ULONG hoff)
@@ -1302,7 +1302,7 @@ inline void CSusie::WriteCollision(ULONG hoff,ULONG pixel)
    RAM_POKE(col_addr,dest);
 
    // Increment cycle count for the read/modify/write
-   cycles_used+=2*SPR_RDWR_CYC;
+   //cycles_used+=2*SPR_RDWR_CYC;
 }
 
 inline ULONG CSusie::ReadCollision(ULONG hoff)
